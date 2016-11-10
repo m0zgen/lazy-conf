792ae6516107d0269925c73dec6084e037f57b5f
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-11-10 19:58:41 +0600

    Add tuned

--- install-general-soft.sh
+++ install-general-soft.sh
@@ -9,8 +9,11 @@
 dnf -y install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Install General use software
-dnf -y install git mc htop nano wget rdesktop arandr tigervnc xclip hunspell-ru ccze youtube-dl ffmpeg nmap alacarte evolution geany gimp freemind vlc kruler plank roxterm wine nscd
+dnf -y install git mc htop nano wget rdesktop arandr tigervnc xclip hunspell-ru ccze youtube-dl ffmpeg nmap alacarte evolution geany gimp freemind vlc kruler plank roxterm wine nscd tuned
 
 # Install Desktops
 dnf config-manager --add-repo http://download.opensuse.org/repositories/home:Pival81/Fedora_23/home:Pival81.repo && dnf -y install vibrancy-colors-icon-theme
 dnf config-manager --add-repo http://download.opensuse.org/repositories/home:Horst3180/Fedora_24/home:Horst3180.repo && dnf -y install arc-theme && dnf -y install gnome-icon-theme
+
+# Enable tuned
+tuned-adm profile balanced
